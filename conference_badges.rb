8d3fc8960a3ba96a92165abd1f6e65377704f371
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-23 23:33:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,10 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array)
-  array.each do |name| badge_maker(name)
+  array.each do |name|
+     return badge_maker(name)
 end 
 
 def assign_rooms(list)
+  list.each do |i|
+    puts "Hello, "
